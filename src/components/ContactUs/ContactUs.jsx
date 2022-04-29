3a56e0ce8d4979214d70aee9dc045739e2db13ee
syrym-umbetov <umbetov.syrym@gmail.com>
2022-04-29 23:58:20 +0600

    contact-us

--- src/components/ContactUs/ContactUs.jsx
+++ src/components/ContactUs/ContactUs.jsx
@@ -1,7 +1,26 @@
 import React from 'react';
 import './ContactUs.css';
+import contactUs from './../../assets/contactus/contact-us.png';
+
 const ContactUs = () => {
-  return <div>ContactUs</div>;
+  return (
+    <div className='contact-us'>
+      <div className='contact-us__image'>
+        <img src={contactUs} alt='' />
+      </div>
+      <div className='contact-us__right'>
+        <div className='right-title'>Contact Us</div>
+        <div className='right-description'>
+          Discover your current English level by taking our free online English
+          test.Sign up to our newsletter for learning tips and free resources
+        </div>
+        <div className='right-buttons'>
+          <input type='text' placeholder='Enter your E-mail' />
+          <button>Subscribe</button>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default ContactUs;
